35a1554694fdbc902a2eae9d0dbddc20e70fbc40
Jackson Pacheco <jackson.pacheco@terceiros.uniasselvi.com.br>
2022-05-27 14:31:34 -0300

    (feat): Implemented analyst fundamental .

--- main.py
+++ main.py
@@ -5,25 +5,7 @@ from yahoo_fin.stock_info import get_analysts_info
 
 config_price = 5
 config_market_cap = 10
-stocks_symbol = sc.get_symbols_br()
+tickerSymbol = 'AAPL'
+tickerData = yf.Ticker(tickerSymbol)
 
-# Pegar as informações
-stock = 'CAAP'
-
-# get data on this ticker
-tickerData = yf.Ticker(stock)
 print(tickerData.info)
-
-df = pd.DataFrame([tickerData.info])
-
-# 1º Preço maior que R$ 5,0
-if df.currentPrice > config_price:
-    # market cap
-    market_cap = df.marketCap / 10000000000
-    market_cap = round(market_cap)
-    # if market_cap > config_market_cap:
-    #     relative_volume = df.volume / df.averageVolume10days
-    #     print(relative_volume)
-
-else:
-    print('Preço abaixo de R$ 5,0')
--- services.py
+++ services.py
@@ -1,10 +1,23 @@
 import pandas as pd
 from yahoo_fin import stock_info as si
-
+import yfinance as yf
 
 def get_symbols_br():
     df = pd.DataFrame(si.tickers_ibovespa())
     return set(symbol for symbol in df[0].values.tolist())
+
+
+def get_symbols_sp500():
+    df = pd.DataFrame(si.tickers_sp500())
+    return set(symbol for symbol in df[0].values.tolist())
+
+
+async def get_data_info():
+    stock = 'ALB'
+    # tickerData = yf.Ticker(stock)
+    # df = pd.DataFrame([tickerData.info])
+    return stock
+
 
 def get_fundamental_values(stocks_symbol):
     for symbol in stocks_symbol:
